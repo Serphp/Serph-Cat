d7bb0a749d155c9f712a8e4ee19281a29201dd8b
Bryan Rodriguez <Serphp@hotmail.com>
2023-05-13 15:40:11 -0500

    [Deploy] #1

--- src/pages/api/Superbase.js
+++ src/pages/api/Superbase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = "https://azdltemsflxeuruxexmi.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImF6ZGx0ZW1zZmx4ZXVydXhleG1pIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODI5OTgxMzAsImV4cCI6MTk5ODU3NDEzMH0.Ak2DB4mccneesHadtU3Q5TwlxkHUbvueFaLGxLMaoX4"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
